604cffdafce3e325b910aacaf25a97fa0c3905f7
Helen <67713010+helenh233@users.noreply.github.com>
2022-12-30 13:06:15 -0500

    Update word_guess.h

--- word_guess.h
+++ word_guess.h
@@ -6,13 +6,13 @@
 // all chars in "result" are either:
 //   lowercase: letter exists in secret, but not in correct position
 //   UPPERCASE: letter in correct position
-//   . (period): letter does not exist in secret word
+//   _ (underscore): letter does not exist in secret word
 
 
 // evaluate_guess(secret, guess, result) returns whether or not secret is equal
 //   to guess and modifies result as described above
 // example:
-//   evaluate_guess("apple", "paper", result) => false; result => "paPe."
+//   evaluate_guess("apple", "paper", result) => false; result -> "paPe_"
 // requires: result is sufficiently large [not asserted]
 //           secret and guess are same length
 // effects: result will be modified
@@ -21,15 +21,15 @@ bool evaluate_guess(const char *secret, const char *guess, char *result);
 
 
 // available_letters(guesses, results, num_guesses, alphabet) determines the
-//   status of letters a...z in the secret word, based on results
-//   of existing guesses; the result is stored in alphabet as an string, where each of a..z is:
-//     . (period), if the letter does not exist in secret word
+//   status of letters 'a'...'z' in the secret word, based on results
+//   of existing guesses; the result is stored in alphabet as an string, where each of 'a'...'z' is:
+//     _ (underscore), if the letter does not exist in secret word
 //     lowercase letter, if the status of the letter is unknown
 //     UPPERCASE letter, if the letter exists in secret word
 // example: char *guesses[2] = { "apple", "touch" };
-//          char *results[2] = { ".pp..", ".OUCH" };
+//          char *results[2] = { "_pp__", "_OUCH" };
 //          available_letters(guesses, results, 2, alphabet);
-//          alphabet => ".bCd.fgHijk.mnOPqrs.Uvwxyz"
+//          alphabet -> "_bCd_fgHijk_mnOPqrs_Uvwxyz"
 // requires: first "num_guesses" elements in guesses and results are valid
 //           strings of the same length
 //           alphabet is sufficiently large [not asserted]
@@ -47,12 +47,12 @@ void available_letters(char *guesses[], char *results[], int num_guesses,
 //    a letter that was previous identified as not part of the secret word
 //    cannot be used in next_guess.
 // examples: char *guesses[1] = { "apple" };
-//           char *results[1] = { ".pp.." };
+//           char *results[1] = { "_pp__" };
 //           valid_guess(guesses, results, 1, "touch") => false
 //           valid_guess(guesses, results, 1, "pouch") => true
 //           valid_guess(guesses, results, 1, "spout") => false
 // requires: first "num_guesses" elements in guesses and results are valid
-//           strings of the same length
+//              strings of the same length
 //           length of next_guess matches length of strings in guesses
 //           num_guesses >= 0
 // time: O(num_guesses * m^2) where m is the length of each guess
